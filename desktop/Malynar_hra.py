d212be80419972c25a0a8bfa994494b79b299e59
libakmatusko <libak.matusko@gmail.com>
2024-05-14 17:31:00 +0200

    vlastne treba dorobit ako budem Smilimu posielat veci co sa daju robit na policku a este prestavavanie budov a uspavanie
    celkom som postupil

--- desktop/Malynar_hra.py
+++ desktop/Malynar_hra.py
@@ -11,7 +11,9 @@ class actions:
         self.name = name
         self.tick_counter = -1
         self.inventory = {
-            'people': 0
+            'people': 0,
+            'stone': 10,
+            'wood': 10
         }
         self.army = {}
         self.trades = []
@@ -31,6 +33,9 @@ class actions:
         self.available_lands = [starting_pos]
         self.add_available_lands(starting_pos)
         self.front = Front(self)
+        with open(f'desktop/buildings.json', 'r') as buildings_file:
+            self.buildings = json.load(buildings_file_file)
+        
 
 
     def tick(self):
@@ -83,6 +88,8 @@ class actions:
                 }
             }
         )
+        if response.status_code == 200:
+            return True
     
 
     def server_upgrade(self, pos: list[int]):
@@ -90,6 +97,76 @@ class actions:
             f'{SERVER_IP}/upgrade/{self.name}',
             json=self.to_pos_string(*pos)
         )
+    
+
+    def build_new(self, build: str, pos: list[int]):
+        building = self.buildings[build]
+        cost = building['cost'][0]
+        requrement = building['requriment']
+        if requrement != '':
+            if not requrement in self.find_resources(pos=pos):
+                return False
+        for c in cost.keys():
+            if cost[c] > self.inventory[c]:
+                return False
+        if not pos in self.available_lands:
+            return False
+        # moze byt postavene
+        self.add_available_lands.remove(pos)
+        self.add_available_lands(pos)
+        for c in cost.keys():
+            self.inventory[c] -= cost[c]
+        self.my_lands.append(
+            {
+                'name': build,
+                'position': pos,
+                'ticks per item': building['ticks per item'][0],
+                'time to generation': building['ticks per item'][0],
+                'generating': building['generating'][0],
+                'input': building['input'][0],
+                'output': building['output'][0],
+                'points': 1
+            }
+        )
+        self.all_lands[self.to_pos_string(*pos)] = {
+            'name': build,
+            'player': self.name,
+            'level': 1
+        }
+        if not self.server_build(pos=pos, building=build):
+            pass# daco ked sa neupdatuje na server
+        return True
+    
+    
+    def cost_to_upgrade(self, pos: list[int]) -> dict:
+        all_building = self.all_lands[self.to_pos_string(*pos)]
+        return self.buildings[all_building['name']]['cost'][all_building['level']]
+    
+    def upgrade(self, pos: list[int]) -> bool:
+        if self.all_lands[self.to_pos_string(*pos)]['player'] != self.name:
+            return False
+        costs = self.cost_to_upgrade(pos=pos)
+        for material in costs.keys():
+            if costs[material] > self.inventory[material]:
+                return False
+        for material in costs.keys():
+            self.inventory[material] -= costs[material]
+        level = self.all_lands[self.to_pos_string(*pos)]['level']
+        self.all_lands[self.to_pos_string(*pos)]['level'] += 1
+        for my_land in self.my_lands:
+            if my_land['position'] == pos:
+                my_land.update({
+                    'ticks per item': building['ticks per item'][level],
+                    'time to generation': building['ticks per item'][level],
+                    'generating': building['generating'][level],
+                    'input': building['input'][level],
+                    'output': building['output'][level],
+                })
+                break
+
+        if not self.server_upgrade(pos=pos):
+            pass# daco ked sa neupdatuje server
+        return True
 
 
     # ak je v inventari dostatok veci, tak ich zobere a vrati True, inak vrati False
@@ -122,8 +199,33 @@ class actions:
             [pos[0]-1, pos[1]],
             [pos[0]+1, pos[1]-1],    
         ]:
-            if not land in self.available_lands:
+            if (not land in self.available_lands) and self.all_lands[self.to_pos_string]['name'] == 'land':
                 self.available_lands.append(land)
+    
+
+    def find_resources(self, pos: list[int]):
+        resources = []
+        for land in [
+            [pos[0], pos[1]+1],
+            [pos[0]+1, pos[1]],
+            [pos[0]-1, pos[1]+1],
+            [pos[0], pos[1]-1],
+            [pos[0]-1, pos[1]],
+            [pos[0]+1, pos[1]-1],    
+        ]:
+            for resource in self.all_lands[to_pos_string(*land)]['resources']:
+                if not resource in resources:
+                    resources.append(resource)
+        return resources
+
+
+    def possible_actions(self, pos):# toto sa este bude zasadne menit
+        if pos in self.available_lands:
+            return ['build']
+        elif self.all_lands[self.to_pos_string(*pos)]['player'] == self.name:
+            return ['upgrade', 'sleep']
+        else:
+            pass
 
 
     # vrati co je na policku inak vrati more
@@ -137,10 +239,10 @@ class actions:
         )
 
     
-    def to_pos_string(x: int, y: int) -> str:
+    def to_pos_string(self, x: int, y: int) -> str:
         return f'{x}x{y}'
 
-    def from_pos_string(pos: str) -> list[int]:
+    def from_pos_string(self, pos: str) -> list[int]:
         return list(map(int, pos.split('x')))
     
 
