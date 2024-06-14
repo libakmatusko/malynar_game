7d41e4797141abc01925db8baf41b41eb84ccd17
piiistik <stevkovasak@gmail.com>
2024-06-15 00:28:26 +0200

    json to pdf

---
+++ admin/json_to_txt.py
@@ -0,0 +1,13 @@
+import json
+
+with open("desktop/buildings.json", "r", encoding='utf-8') as f:
+    buildings = json.load(f)
+
+with open("admin/builds_txt.txt", "w", encoding="utf-8") as f:
+    for build in buildings.keys():
+        print(build)
+        if build == 'road':
+            continue
+        f.write('\n' + build.upper() + '\n')
+        for i in range(len(buildings[build]['cost'])):
+            f.write(f'{buildings[build]["input"][i]}    ---({buildings[build]["ticks per item"][i]} ⌛)--->    {buildings[build]["output"][i]}\n')
