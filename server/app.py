31151a134a1fed611886f475395cf3ae807baea1
piiistik <stevkovasak@gmail.com>
2024-06-15 13:14:39 +0200

    server changes

--- server/app.py
+++ server/app.py
@@ -199,5 +199,5 @@ def from_pos_string(pos: str):
 
 game = game_state('idk')
 
-#if __name__ == '__main__':
-#    app.run(host = '0.0.0.0', port = 5000)
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 5000)
